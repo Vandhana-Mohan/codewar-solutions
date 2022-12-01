a725f708fad0daf25e9e1a301eeebd1eb31a37f5
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-30 23:21:33 -0500

    new code updated

--- 8 kyu/Not so black box.js
+++ 8 kyu/Not so black box.js
@@ -2,7 +2,7 @@
  * DESCRIPTION: 
  * 
  * The kata has been preloaded with a function called blackBox. Figure out what the function does.
- * You may need to think laterally!
+ * You may need to think laterally!!!
  */
 
 
