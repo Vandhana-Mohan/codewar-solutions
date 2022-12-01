91fbad29f86c09309116bf1649b120b2be031989
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-30 23:19:26 -0500

    new code updated

---
+++ 8 kyu/Not so black box.js
@@ -0,0 +1,14 @@
+/**
+ * DESCRIPTION: 
+ * 
+ * The kata has been preloaded with a function called blackBox. Figure out what the function does.
+ * You may need to think laterally!
+ */
+
+
+ // My Solution
+
+ JSopenSesame()
+ blackBox();
+
+ // Keep Coding...
