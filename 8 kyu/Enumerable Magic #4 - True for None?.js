d95a30f4b29ecc4b28c932679e01eb20900e3d5d
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-30 22:38:24 -0500

    new code updated

---
+++ 8 kyu/Enumerable Magic #4 - True for None?.js
@@ -0,0 +1,34 @@
+ /**
+  * DESCRIPTION:
+  * 
+  * Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns 
+  * true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+  * 
+  * Explanation:
+  * 
+  * Returns a boolean if the function returns true
+  * @param {[]} arr 
+  * @param {function} fun 
+  * @returns boolean
+  */
+
+ // My Solution
+
+ const none = (arr, fun) => {  
+    for ( let item of arr )  { // loop through `arr` to see if it has any items
+      if ( fun(item) ) {      // check `fun` for `item` - if `fun` has `item`, return false.
+        return false;
+      }
+   }
+  return true; // outside if for loop fails
+  }
+
+ // Best Practice Solution (Recommended)
+
+  const none = (arr, fun) => !arr.some(fun)
+
+ // Clever Solution
+ 
+  const none = (arr, fun) => arr.filter(fun).length === 0;
+
+  // Keep Coding...
