c46a5b15c59da034cd59dab6e463a86042367fdd
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-03 01:12:03 -0500

    new code uploaded

--- 8 kyu/Sort and Star.js
+++ 8 kyu/Sort and Star.js
@@ -1,10 +1,29 @@
-/**
+ /**
  * DESCRIPTION:
-You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+ * 
+ * You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of 
+ * the chars) and then return the first value.
 
-The returned value must be a string, and have "***" between each of its letters.
+ * The returned value must be a string, and have "***" between each of its letters.
 
-You should not remove or add elements from/to the array.
-
-
+ * You should not remove or add elements from/to the array.
  */
+
+ // My Solution
+
+ function twoSort(s) {
+    s.sort();
+    return s[0].split('').join('***');
+ }
+ 
+ // Best Practice Solution (Recommended)
+
+ const twoSort = (s) => s.sort()[0].split('').join('***');
+
+
+ // Clever Solution
+ 
+ const twoSort = s => [...s.sort()[0]].join('***');
+
+
+ 
